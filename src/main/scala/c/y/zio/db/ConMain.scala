8fe430357b1a42e389ed257e2a331a4032c53d64
ygkim <gonni21c@gmail.com>
2023-11-24 18:01:37 +0900

    daily commit

---
+++ src/main/scala/c/y/zio/db/ConMain.scala
@@ -0,0 +1,33 @@
+package c.y.zio.db
+
+import io.getquill._
+
+object ConMain {
+  def main(args: Array[String]): Unit = {
+//    val ctx = new SqlMirrorContext(MirrorSqlDialect, Literal)
+//    import ctx._
+//
+//    val pi = quote(3.14159)
+//
+//    final case class Circle(radius: Float)
+//
+//    val areas = quote {
+//      query[Circle].map(c => pi * c.radius * c.radius)
+//    }
+//
+//    val area = quote {
+//      (c: Circle) => {
+//        val r2 = c.radius * c.radius
+//        pi * r2
+//      }
+//    }
+//
+//    implicit final class Ext(private val q: Query[Person]) extends AnyVal {
+//      def olderThan(age: Int) = quote {
+//        query[Person].filter(p => p.age > lift(age))
+//      }
+//    }
+//
+//    run(query[Person].olderThan(22))
+  }
+}
--- src/main/scala/c/y/zio/db/SamMain.scala
+++ src/main/scala/c/y/zio/db/SamMain.scala
@@ -6,21 +6,42 @@ import zio._
 
 import java.sql.SQLException
 
-case class Person(name: String, age: Int)
+
+case class Employee(pid: Int, name: String, age: Int)
 
 class DataService(quill: Quill.Mysql[SnakeCase]) {//Postgres[SnakeCase]) {
   import quill._
-  def getPeople: ZIO[Any, SQLException, List[Person]] = run(query[Person])
+  def getPeople: ZIO[Any, SQLException, List[Employee]] = run(query[Employee])
+
+  def insertPeople(name: String, age: Int): ZIO[Any, SQLException, Int] = {
+    val q = quote {
+      query[Employee].insertValue(lift(Employee(0, name, age))).returningGenerated(r => r.pid)
+    }
+    run(q)
+  }
+
 }
 object DataService {
-  def getPeople: ZIO[DataService, SQLException, List[Person]] =
+  def getPeople: ZIO[DataService, SQLException, List[Employee]] =
     ZIO.serviceWithZIO[DataService](_.getPeople)
+
+  def insertPeople(name: String, age: Int) = ZIO.serviceWithZIO[DataService](_.insertPeople(name, age))
 
   val live = ZLayer.fromFunction(new DataService(_))
 }
+
 object Main extends ZIOAppDefault {
+
+  val program = for {
+    _ <- DataService.getPeople.debug
+    res <- DataService.insertPeople("Jane", 17).debug
+    _ <- DataService.getPeople.debug
+  } yield ()
+
   override def run = {
-    DataService.getPeople
+//    DataService.getPeople
+    println(program)
+    program
       .provide(
         DataService.live,
         Quill.Mysql.fromNamingStrategy(SnakeCase),
